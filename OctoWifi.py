a994d05affacc173bfb2324eb885d1c24d63b143
Solomon Roberts <badger@solomons-mini.local>
2014-07-02 22:23:40 -0600

    adjusted gpio to __init__, added rough ws framework

--- OctoWifi.py
+++ OctoWifi.py
@@ -5,20 +5,21 @@ from wifi import Cell, Scheme
 import time
 
 class OctoWifi(object):
-    
+
     def __init__(self):
         self.networks = []
         self._setup()
-        
+        self.gpio_pin = 26
+
     def _setup(self):
         gpio.setmode(gpio.BOARD)
-        gpio.setup(26, gpio.IN, pull_up_down=gpio.PUD_UP)
+        gpio.setup(self.gpio_pin, gpio.IN, pull_up_down=gpio.PUD_UP)
 
     def get_networks(self):
         '''get a list of networks...'''
         interface = 'wlan0'
         return Cell.all(interface)
-    
+
     def dict_networks(self, network_list):
         '''build a dictionary object out of network information'''
         ap = {}
@@ -29,29 +30,28 @@ class OctoWifi(object):
             nw['encryption'] = item.encryption_type
             nw['mac_addr'] = item.address
             ap['network'] = nw
-            
+
         print ap
-        
+
     def list_networks(self):
         if len(self.networks) > 0:
             return self.networks
         else:
             self.networks = self.get_networks()
             return self.networks
-        
+
     def run(self):
         while True:
             print self.list_networks()
             print self.gpio_stuff()
             time.sleep(10)
-            
-    def gpio_stuff(self):
-        while True:                                      
-            if (gpio.input(26) == 0):
-                return 'client'
-            else:
-                return "AP"
-        
+
+    def gpio_check(self):
+        if (gpio.input(self.gpio_pin) == 0):
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
     #print 'this is a module, dont run it on its own'
---
+++ octowifi_server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# pylint: disable=W0611,W0612,R0912
+
+
+from time import sleep
+import threading
+from bottle import route, run, response, post, request, get
+import json
+import logging
+from common import jsondump
+from datetime import datetime
+
+
+class WS(threading.Thread):
+    def __init__(self, main):
+        self.main = main
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.setName("WEBSVC")
+        while self.main.shutdown is False:
+            sleep(1)
+            logging.info("Starting Webservice")
+            self.server()
+
+    def server(self):
+        @route('/')
+        def index():
+            return "generic application - WS"
+
+        @route('/nagios')
+        def nagios():
+            status = {"status_code": 0, "status_msg": "OK (static)"} # fixme
+            return jsondump(status)
+
+        @get('/status')
+        def status():
+            return jsondump(self.main._status)
+
+        @get('/shutdown/now')
+        def shutdownnow():
+            logging.info("Shutdown Called from WS")
+            self.main.shutdown = True
+
+        while True:
+            try:
+                run(host='0.0.0.0', port=int(self.main.cfg.ws['port']), quiet=True)
+            except Exception as e:
+                sleep(2)
+                logging.critical(e, exc_info=1)
