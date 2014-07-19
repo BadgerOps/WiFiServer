e13c96b7efb9e07ed651588dfd2bcb8d278a6a4b
Jesse Roberts <jesse@hackedpotatoes.com>
2014-07-19 16:56:22 -0600

    SVC object

---
+++ common/svc.py
@@ -0,0 +1,17 @@
+import logging
+
+
+class WiFiObj(object):
+    """
+    Common object to inherit from, provides access to services/information
+    """
+    svc = None
+
+
+class SVC(object):
+    """
+    Used by WiFiObj to actually provide services/information to child objects
+    """
+    def __init__(self):
+        self.cfg = None
+        self.apmode = True
