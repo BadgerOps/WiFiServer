027dbea2717fb6e767a7dd85a68502b62e5afa6f
Sol Roberts <badger32d@gmail.com>
2014-08-03 20:53:40 -0600

    moved rpi hw interaction to its own file

---
+++ common/rpi_hw.py
@@ -0,0 +1,24 @@
+import logging
+try:
+    import RPi.GPIO as gpio
+except RuntimeError:
+    print "Must be run on a raspberry pi"
+except Exception:
+    print 'cannot import the RPi.GPIO lib, is it installed?'
+
+
+class RpiHW(object):
+
+    def __init__(self):
+         self.gpio_pin = 26  # FIXME: this shouldn't be here, move to cfg
+
+    def _setup(self):
+        gpio.setmode(gpio.BOARD)
+        gpio.setup(self.gpio_pin, gpio.IN, pull_up_down=gpio.PUD_UP)
+
+
+    def gpio_check(self):
+            if (gpio.input(self.gpio_pin) == 0):
+                return True
+            else:
+                return False
--- common/wificlient.py
+++ common/wificlient.py
@@ -8,12 +8,6 @@ import os
 import sys
 import configparser
 from time import sleep
-try:
-    import RPi.GPIO as gpio
-except RuntimeError:
-    print "Must be run on a raspberry pi"
-except Exception:
-    print 'cannot import the RPi.GPIO lib, is it installed?'
 
 
 class WifiClient(object):
@@ -21,12 +15,9 @@ class WifiClient(object):
     def __init__(self):
         self.networks = []
         #self._setup()
-        #self.gpio_pin = 26
 
     def _setup(self):
         pass
-        # gpio.setmode(gpio.BOARD)
-        # gpio.setup(self.gpio_pin, gpio.IN, pull_up_down=gpio.PUD_UP)
 
     def get_networks(self):
         '''get a list of networks...'''
@@ -43,8 +34,7 @@ class WifiClient(object):
             nw['encryption'] = item.encryption_type
             nw['mac_addr'] = item.address
             ap['network'] = nw
-
-        print ap
+        return ap
 
     def list_networks(self):
         if len(self.networks) > 0:
@@ -52,15 +42,3 @@ class WifiClient(object):
         else:
             self.networks = self.get_networks()
             return self.dict_networks(self.networks)
-
-    def run(self):
-        while True:
-            print self.list_networks()
-            #print self.gpio_stuff()
-            time.sleep(10)
-
-    def gpio_check(self):
-        if (gpio.input(self.gpio_pin) == 0):
-            return True
-        else:
-            return False
