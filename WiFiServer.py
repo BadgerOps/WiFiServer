50aa8c0523be0b64b6db978a606324c32aac4581
Sol Roberts <badger32d@gmail.com>
2014-08-05 22:50:00 -0600

    now with more content

---
+++ WiFiServer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import logging
+import sys
+import time
+from datetime import datetime
+
+class WiFiServer(object):
+
+    def __init__(self, cfg=None):
+        self.setup_logging()
+        self.shutdown = False
+
+
+    def setup_logging(self):
+        """Setup Logging"""
+        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        logging.StreamHandler(sys.stdout)
+
+    def keyboardinterrupt(self):
+        self.shutdown = True
+        logging.info("KeyboardInterrupt Called, Shutting down application")
+
+    def start(self):
+        self.main()
+
+    def main(self):
+        """main thread"""
+        try:
+            logging.info("Main Thread Stable (startup complete)")
+            while self.shutdown == False:
+                try:
+                    time.sleep(15)
+                    logging.debug('in the main loop')
+                    print "main loop"
+                    #self.set_threadstatus("MAIN", "LOOP")
+                    ## do stuff
+                except KeyboardInterrupt:
+                    self.keyboardinterrupt()
+            # self.set_threadstatus("MAIN", "CLEANUP")
+            # self._status['status'] = 'cleanup'
+            logging.info("Begin Shutdown Sequeuence")
+            #self.cleanup()
+            logging.info("WiFiServer is Shutdown")
+        except Exception as e:
+            logging.critical("Error in Main loop: {0}".format(e))
+        finally:
+            exit()
